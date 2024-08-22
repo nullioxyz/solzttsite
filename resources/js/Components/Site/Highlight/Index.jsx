c8f64a712eca7ff29265a944ef8d6476f0e2f566
frankedeveloper@gmail.com <frankedeveloper@gmail.com>
2024-08-22 18:58:21 +0200

    Add solztt logo animation

--- resources/js/Components/Site/Highlight/Index.jsx
+++ resources/js/Components/Site/Highlight/Index.jsx
@@ -10,11 +10,11 @@ export default function Highlight() {
   };
 
   return (
-    <section className="flex flex-col justify-between items-center max-w-[1240px] mx-auto text-white" id="highlight">
-      <div className="highlight flex justify-center items-center">
+    <section className="flex flex-col items-center max-w-[1240px] mx-auto text-white" id="highlight">
+      <div className="highlight mt-20">
         <Logo />
       </div>
-      <div className="animate-bounce mt-40 cursor-pointer" onClick={scrollToNextSection}>
+      <div className="animate-bounce lg:mt-[-50px] md:mt-[-40px] xs:mt-20 custom:mt-64 cursor-pointer" onClick={scrollToNextSection}>
         <svg className="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
         </svg>
