bad071de4bd89920a54aecb7dabcc871c8d4af3d
frankedeveloper@gmail.com <frankedeveloper@gmail.com>
2024-09-04 14:08:26 +0200

    Feat loading

---
+++ resources/js/Components/Loader/Index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Loader = ({ progress, fadingOut }) => (
+  <div
+    className={`fixed inset-0 bg-white bg-opacity-80 flex items-center justify-center z-50 transition-opacity duration-1000 ${fadingOut ? 'opacity-0' : 'opacity-100'}`}
+    aria-busy="true"
+    aria-label="Carregando"
+  >
+    <div className="text-black text-center">
+      <div className="mt-2 w-10 h-10 border-4 border-black border-t-transparent rounded-full animate-spin"></div>
+      <div className="mt-4 text-lg">{progress}%</div>
+    </div>
+  </div>
+);
+
+export default Loader;
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -6,17 +6,70 @@ import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import './i18n';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+import React, { useState, useEffect } from 'react';
+
+import Loader from './Components/Loader/Index';
+
+const appName = 'Solztt';
+
+function InertiaAppWrapper({ App, props }) {
+  const [loading, setLoading] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [fadingOut, setFadingOut] = useState(false);
+
+
+  const { initialPage } = props;
+
+  const shouldShowLoader = initialPage.component === 'Site/Index';
+
+  useEffect(() => {
+    if (shouldShowLoader) {
+      setLoading(true);
+      const interval = setInterval(() => {
+        setProgress((prevProgress) => {
+          if (prevProgress >= 100) {
+            clearInterval(interval);
+            setFadingOut(true);
+            setTimeout(() => {
+              setLoading(false);
+              setFadingOut(false);
+            }, 1000);
+            return 100;
+          }
+          return prevProgress + 1;
+        });
+      }, 30);
+
+      return () => clearInterval(interval);
+    } else {
+      setLoading(false);
+    }
+  }, [shouldShowLoader]);
+
+  return (
+    <>
+      {loading ? (
+        <Loader progress={progress}  fadingOut={fadingOut} />
+      ) : (
+        <App {...props} />
+      )}
+    </>
+  );
+}
+
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
-    setup({ el, App, props }) {
-        const root = createRoot(el);
+  title: (title) => `${title} - ${appName}`,
+  resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+  setup({ el, App, props }) {
+    const root = createRoot(el);
 
-        root.render(<App {...props} />);
-    },
-    progress: {
-        color: '#4B5563',
-    },
+    // Renderizando o InertiaAppWrapper, que agora gerencia o estado de loading
+    root.render(<InertiaAppWrapper App={App} props={props} />);
+  },
+  progress: {
+    color: 'red',
+    delay: 2500,
+    showSpinner: true
+  },
 });
