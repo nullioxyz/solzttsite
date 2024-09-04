66b897cc5b9e41974728eb442996eb9d30ffb072
frankedeveloper@gmail.com <frankedeveloper@gmail.com>
2024-09-04 16:14:54 +0200

    Add google analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,25 +1,43 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-        
-        
-        <link rel="stylesheet" href="{{ asset('./css/fonts.css') }}"  />
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Solztt') }}</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
 
-        <!-- Scripts -->
-        @routes
-        @viteReactRefresh
-        @vite(['resources/js/app.jsx'])
-        @inertiaHead
-    </head>
-    <body class="antialiased">
-        @inertia
-    </body>
+
+    <link rel="stylesheet" href="{{ asset('./css/fonts.css') }}" />
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-L1M0C8JWXT"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-L1M0C8JWXT');
+    </script>
+
+
+    <title inertia>{{ config('app.name', 'Solztt') }}</title>
+
+    <!-- Scripts -->
+    @routes
+    @viteReactRefresh
+    @vite(['resources/js/app.jsx'])
+    @inertiaHead
+</head>
+
+<body class="antialiased">
+    @inertia
+</body>
+
 </html>
