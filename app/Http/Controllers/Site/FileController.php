a17b2b4fb40f721404b3bc2ad57f446fc8f5a263
frankedeveloper@gmail.com <frankedeveloper@gmail.com>
2024-09-09 11:51:09 +0200

    Optmize filecontroller

--- app/Http/Controllers/Site/FileController.php
+++ app/Http/Controllers/Site/FileController.php
@@ -11,17 +11,33 @@ class FileController extends Controller
 {
     public function index(Request $request)
     {
+        // Busca a mídia pelo UUID
         $media = Media::findByUuid($request->uuid);
         
-        if (Storage::disk('s3')->exists($media->getPath())) {
-            $file = Storage::disk('s3')->get($media->getPath());
-            
+        if (!$media) {
+            abort(404);
+        }
+
+        $disk = $media->disk;
+
+        if (Storage::disk($disk)->exists($media->getPath())) {
+            $file = Storage::disk($disk)->get($media->getPath());
+
             $finfo = new \finfo(FILEINFO_MIME_TYPE);
             $mimeType = $finfo->buffer($file);
-            
+
             return response($file, 200)->header('Content-Type', $mimeType);
-        } else {
-            abort(404);
+        }
+
+        $defaultImagePath = 'images/default.png';
+
+        if (Storage::disk('local')->exists($defaultImagePath)) {
+            $file = Storage::disk('local')->get($defaultImagePath);
+
+            $finfo = new \finfo(FILEINFO_MIME_TYPE);
+            $mimeType = $finfo->buffer($file);
+
+            return response($file, 200)->header('Content-Type', $mimeType);
         }
     }
 }
