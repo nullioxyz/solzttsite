73bf673e2c916e73eb1357ba95004bc1a86c2d00
frankedeveloper@gmail.com <frankedeveloper@gmail.com>
2025-04-19 19:46:15 +0200

    Fix arrow Carousel

--- resources/js/Components/Site/Components/Gallery.jsx
+++ resources/js/Components/Site/Components/Gallery.jsx
@@ -1,8 +1,31 @@
-import { Carousel } from "@material-tailwind/react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
+import { Carousel, IconButton } from "@material-tailwind/react";
  
 export function Gallery({ images}) {
   return (
     <Carousel
+      prevArrow={({ handlePrev }) => (
+        <IconButton
+          variant="text"
+          color="black"
+          size="lg"
+          onClick={handlePrev}
+          className="!absolute top-2/4 left-4 -translate-y-2/4 bg-black/40 hover:bg-black/60"
+        >
+          <ChevronLeftIcon className="h-6 w-6 text-white" />
+        </IconButton>
+      )}
+      nextArrow={({ handleNext }) => (
+        <IconButton
+          variant="text"
+          color="black"
+          size="lg"
+          onClick={handleNext}
+          className="!absolute top-2/4 right-4 -translate-y-2/4 bg-black/40 hover:bg-black/60"
+        >
+          <ChevronRightIcon className="h-6 w-6 text-white" />
+        </IconButton>
+      )}
       loop={true} autoplay={false}
       className="rounded-lg relative w-full">
       {images.map((image, index) => (
