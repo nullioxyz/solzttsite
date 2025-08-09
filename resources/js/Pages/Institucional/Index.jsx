b8a97ba00583581e3545d5be9a16a9d517782304
frankedeveloper@gmail.com <frankedeveloper@gmail.com>
2025-08-09 20:09:34 +0200

    Remove create buttom from institucional

--- resources/js/Pages/Institucional/Index.jsx
+++ resources/js/Pages/Institucional/Index.jsx
@@ -13,12 +13,6 @@ export default function Index({ auth, institucionals  }) {
             <Head title="Institucional" />
 
             <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-                    <div className="flex justify-end mb-4">
-                        <AddButton url={route('institucional.create')} />
-                    </div>
-                </div>
-            
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <List items={institucionals.data}/>
                 </div>
