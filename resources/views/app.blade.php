1b26dd3145678f5b111237ebb8ef318d77eef3ea
Rafael Franke <frankerafael@bikeflip.com>
2025-09-20 12:12:17 +0200

    meta title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,8 +10,7 @@
                 ? $page['props']['meta_title'].' | '.config('app.name') 
                 : (($page['props']['metatags']['translation']['title'] 
                     ?? $page['props']['metatags']['default_translation']['title'] 
-                    ?? config('app.name'))
-                    .' | '.config('app.name')) }}</title>
+                    ?? config('app.name'))) }}</title>
 
         <meta name="description" content="{{ isset($page['props']['meta_description']) 
                 ? strip_tags($page['props']['meta_description']) 
@@ -29,8 +28,7 @@
                 ? $page['props']['meta_title'].' | '.config('app.name') 
                 : (($page['props']['metatags']['translation']['title'] 
                     ?? $page['props']['metatags']['default_translation']['title'] 
-                    ?? config('app.name'))
-                    .' | '.config('app.name')) }}">
+                    ?? config('app.name'))) }}">
 
 
         <meta property="og:description" 
@@ -52,8 +50,7 @@
                 ? $page['props']['meta_title'].' | '.config('app.name') 
                 : (($page['props']['metatags']['translation']['title'] 
                     ?? $page['props']['metatags']['default_translation']['title'] 
-                    ?? config('app.name'))
-                    .' | '.config('app.name')) }}">
+                    ?? config('app.name'))) }}">
                     
         <meta name="twitter:description" content="{{ isset($page['props']['meta_description']) 
                 ? strip_tags($page['props']['meta_description']) 
